21e0c132987c242d6af99cc5ff467b808f4af3f0
Tahaanaribinjaved <iamtahaansari293@gmail.com>
2025-11-09 23:55:27 +0500

    1 dayto comack

---
+++ js assignment/SMIT-batch-9-master/JavaScript/Js assignment solved/app.js
@@ -0,0 +1,44 @@
+        //! 1.Alerts
+
+// 1.	Write a script to greet your website visitor using JS alert box.
+// alert("Wellcome User!");/
+// 2.	Write a script to display following message on your web page:
+// alert("Error! Please enter a valid password.");
+// 3.	Write a script to display following message on your web page: (Hint : Use line break)
+// alert("Wellcome to JS Land \n Happy coding ");
+// 4.	Write a script to display following messages in sequence:
+// alert("Wellcome to JS land");
+// alert("Happy coding");
+        //! 2.VARIABLES	FOR	STRINGS
+
+// 1. Declare a variable called username.
+// var username;
+// 2. Declare a variable called myName & assign to it a string
+// that represents your Full Name.
+// var myName = "Taha ansari"
+// 3. Write a script to
+// a. declare a JS variable, titled message
+// b. assign “Hello World” to the variable message
+// c. display the message in alert box
+// 4. Write a script to save student’s bio data in JS variables
+// and show the data in alert boxes. Example:
+// VARIABLES	
+//   FOR	
+//   STRINGS |	
+//   JAVASCRIPT
+// Page	
+//   2 of	
+//   4
+// 5. Write a script to display the following alert using one JS
+// variable:
+ // Single string variable with newline characters
+    // const message = "JavaScript Alert\n\nPIZZA\nPIZZ\nPIZ\nPI\nP";
+
+    // Display the alert
+    // alert(message);
+        //! 3.VARIABLES	FOR	  NUMBERS
+// 3. Declare a variable called birthYear & assign to it your
+// birth year. Show the following message in your browse
+
+// var birthYear = 2007;
+// alert("My birth year is " + birthYear+ "\ndataType of my declared variable is "+typeof(birthYear));
